37b2ddd66a82eb6da48fd4ce37e8389ac15cad45
lservo <lucas.servo@paipe.co>
2023-08-03 21:28:25 -0300

    chore: initial commit

---
+++ dist/index.js
@@ -0,0 +1,99 @@
+#!/usr/bin/env node
+"use strict";
+/* eslint-disable @typescript-eslint/no-var-requires */
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var node_fs_1 = __importDefault(require("node:fs"));
+var node_path_1 = require("node:path");
+var generator_helper_1 = require("@prisma/generator-helper");
+var sdk_1 = require("@prisma/sdk");
+function findEncryptFields(filePath) {
+    var fileContent = node_fs_1.default.readFileSync(filePath, "utf-8");
+    var lines = fileContent.split("\n");
+    var commentRegex = /\/\/.*?@encrypt\b/;
+    var modelRegex = /^\s*model\s+(\w+)/;
+    var modelsEncryptedFields = {};
+    var currentModel = null;
+    lines.forEach(function (line) {
+        var modelMatch = line.match(modelRegex);
+        if (modelMatch)
+            currentModel = modelMatch[1];
+        var commentMatch = line.match(commentRegex);
+        if (commentMatch && currentModel) {
+            var _a = line.split(/\s+/).filter(Boolean), fieldName = _a[0], typeName = _a[1];
+            if (!typeName.includes("String")) {
+                sdk_1.logger.error("@encrypt is only supported for String fields. Field ".concat(currentModel, ".").concat(fieldName, " is ").concat(typeName, "."));
+                process.exit(1); // Encerra o processo com um código de erro (1)
+            }
+            if (!modelsEncryptedFields[currentModel])
+                modelsEncryptedFields[currentModel] = [];
+            modelsEncryptedFields[currentModel].push({ fieldName: fieldName, typeName: typeName });
+        }
+    });
+    return modelsEncryptedFields;
+}
+(0, generator_helper_1.generatorHandler)({
+    onManifest: function () {
+        return {
+            version: "0.0.1",
+            // defaultOutput: "node_modules/@prisma-client",
+            defaultOutput: "./",
+            prettyName: "Encrypt Fields",
+        };
+    },
+    onGenerate: function (options) {
+        return __awaiter(this, void 0, void 0, function () {
+            var encryptedFields, encryptedFieldsJSON, fileContent, outputFilePath;
+            return __generator(this, function (_a) {
+                encryptedFields = findEncryptFields(options.schemaPath);
+                encryptedFieldsJSON = JSON.stringify(encryptedFields, null, 4);
+                fileContent = "interface IPrismaEncryptFields {\n            [key: string]: {\n                fieldName: string;\n                typeName: string;\n            }[];\n        }\n        \n        export const prismaEncryptFields: IPrismaEncryptFields = ".concat(encryptedFieldsJSON, ";\n");
+                outputFilePath = options.generator.output.value ||
+                    (0, node_path_1.resolve)("src/database/prisma/prisma-generator-encrypt/", "encrypt-fields.ts");
+                node_fs_1.default.writeFileSync(outputFilePath, fileContent, "utf-8");
+                sdk_1.logger.info("Generated ".concat(outputFilePath));
+                return [2 /*return*/, {
+                        exitCode: 0,
+                    }];
+            });
+        });
+    },
+});
---
+++ src/index.ts
@@ -0,0 +1,83 @@
+#!/usr/bin/env node
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+import fs from "node:fs";
+import { resolve } from "node:path";
+
+import { generatorHandler, GeneratorOptions } from "@prisma/generator-helper";
+import { logger } from "@prisma/sdk";
+
+function findEncryptFields(filePath: string) {
+    const fileContent = fs.readFileSync(filePath, "utf-8");
+    const lines = fileContent.split("\n");
+
+    const commentRegex = /\/\/.*?@encrypt\b/;
+    const modelRegex = /^\s*model\s+(\w+)/;
+
+    const modelsEncryptedFields = {};
+
+    let currentModel: string = null;
+
+    lines.forEach((line) => {
+        const modelMatch = line.match(modelRegex);
+        if (modelMatch) [, currentModel] = modelMatch;
+
+        const commentMatch = line.match(commentRegex);
+        if (commentMatch && currentModel) {
+            const [fieldName, typeName] = line.split(/\s+/).filter(Boolean);
+
+            if (!typeName.includes("String")) {
+                logger.error(
+                    `@encrypt is only supported for String fields. Field ${currentModel}.${fieldName} is ${typeName}.`,
+                );
+                process.exit(1); // Encerra o processo com um código de erro (1)
+            }
+
+            if (!modelsEncryptedFields[currentModel])
+                modelsEncryptedFields[currentModel] = [];
+
+            modelsEncryptedFields[currentModel].push({ fieldName, typeName });
+        }
+    });
+
+    return modelsEncryptedFields;
+}
+
+generatorHandler({
+    onManifest() {
+        return {
+            version: "0.0.1",
+            // defaultOutput: "node_modules/@prisma-client",
+            defaultOutput: "./",
+            prettyName: "Encrypt Fields",
+        };
+    },
+    async onGenerate(options: GeneratorOptions) {
+        const encryptedFields = findEncryptFields(options.schemaPath);
+
+        const encryptedFieldsJSON = JSON.stringify(encryptedFields, null, 4);
+
+        const fileContent = `interface IPrismaEncryptFields {
+            [key: string]: {
+                fieldName: string;
+                typeName: string;
+            }[];
+        }
+        
+        export const prismaEncryptFields: IPrismaEncryptFields = ${encryptedFieldsJSON};\n`;
+
+        const outputFilePath =
+            options.generator.output.value ||
+            resolve(
+                "src/database/prisma/prisma-generator-encrypt/",
+                "encrypt-fields.ts",
+            );
+
+        fs.writeFileSync(outputFilePath, fileContent, "utf-8");
+
+        logger.info(`Generated ${outputFilePath}`);
+        return {
+            exitCode: 0,
+        };
+    },
+});
