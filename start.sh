e2db9fd37f27f6eab01dbd26a8a80be7095f9151
lservo <lucas.servo@paipe.co>
2023-08-22 19:58:18 -0300

    chore: added command to restore prisma/schema.prisma after prisma db pull

--- start.sh
+++ start.sh
@@ -2,3 +2,4 @@ npm run build
 docker compose --file ./test/compose.yaml up -d --wait db_test
 npx prisma migrate deploy --schema=./test/schema.prisma
 npx prisma generate --generator client --generator prisma_crypto  --schema=./test/schema.prisma
+git restore prisma/schema.prisma
